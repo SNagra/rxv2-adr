af4c72707d61cb24e42c640bc7589e314af93a55
Sunny Nagra <snagra045@gmail.com>
2014-11-11 23:36:17 -0800

    initial commit

---
+++ js/legend.js
@@ -0,0 +1,21 @@
+function legend(parent, data) {
+    parent.className = 'legend';
+    var datas = data.hasOwnProperty('datasets') ? data.datasets : data;
+
+    // remove possible children of the parent
+    while(parent.hasChildNodes()) {
+        parent.removeChild(parent.lastChild);
+    }
+
+    datas.forEach(function(d) {
+        var title = document.createElement('span');
+        title.className = 'title';
+        title.style.backgroundColor = d.hasOwnProperty('strokeColor') ? d.strokeColor : d.color;
+        parent.appendChild(title);
+
+        var text = document.createTextNode(d.label);
+        var value = document.createTextNode(": " + d.value);
+        title.appendChild(text);
+        title.appendChild(value);
+    });
+}
